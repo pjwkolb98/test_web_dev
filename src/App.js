5f29af066b8ce5d45e487cfb9917a2be42d445a4
pjwko <pjwkolb98@gmail.com>
2020-07-22 17:46:18 +0200

    first change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World - Init
+          Hello World - first change
         </p>
         <a
           className="App-link"
